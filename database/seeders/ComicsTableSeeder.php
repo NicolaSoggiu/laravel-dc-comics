0348ec618a5d76bc2135fe1355b38d37aaf5be28
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-06-29 15:29:35 +0200

    add migration and model Comic

---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comic;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach (config("comics") as $comic) {
+            Comic::create($comic);
+        }
+        
+    }
+}
