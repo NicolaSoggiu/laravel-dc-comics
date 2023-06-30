f424585a1dd0e8266dabb3ea29d96fed64370fee
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-06-30 14:19:33 +0200

    bonus

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.base')
+
+@section('contents')
+
+<section class="bg-dark ">
+    <h1 class="text-center text-warning mt-5 pt-5">Something went wrong, we didn't find what you were looking for.
+        Try again!
+    </h1>
+
+    <button class="btn btn-primary m-5">
+        <a class="text-light text-decoration-none" href="http://127.0.0.1:8000">Back to Home</a>
+    </button>
+</section>
+@endsection
